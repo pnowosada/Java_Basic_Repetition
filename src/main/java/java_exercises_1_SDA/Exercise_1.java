e05ec82a51ca89b808cbaddfa0a7851f9dc26b07
Przemek <przem.nowosada@gmail.com>
2020-12-19 16:52:15 +0100

    Exercise1_1SDA

---
+++ src/main/java/java_exercises_1_SDA/Exercise_1.java
@@ -0,0 +1,14 @@
+package java_exercises_1_SDA;
+
+public class Exercise_1 {
+    public static void multiplicationTable(int number) {
+        for(int i=1;i<=number;i++){
+            int result=i*number;
+            System.out.println(number+"x"+i+"="+result);
+        }
+    return ;
+
+    }
+
+
+}
---
+++ src/main/java/java_exercises_1_SDA/Main_1SDA.java
@@ -0,0 +1,8 @@
+package java_exercises_1_SDA;
+
+public class Main_1SDA {
+    public static void main(String[] args) {
+      Exercise_1.multiplicationTable(5);
+
+    }
+}
