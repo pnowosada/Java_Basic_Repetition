1f4642776642e04f453f85c65f39805b9b8ed9ea
Przemek <przem.nowosada@gmail.com>
2020-12-20 14:49:02 +0100

    Exercise2_1SDA

--- src/main/java/java_exercises_1_SDA/Exercise_2.java
+++ src/main/java/java_exercises_1_SDA/Exercise_2.java
@@ -1,2 +1,21 @@
-package java_exercises_1_SDA;public class Exercise_2 {
+package java_exercises_1_SDA;
+
+public class Exercise_2 {
+    public static void checkingPrimeNumber(int number) {
+        boolean isPrime=true;
+        for(int i=2;i<number;i++){
+            if(number%i==0) {
+                isPrime = false;
+                break;
+            }
+
+        }
+        if(isPrime){
+            System.out.println("This is prime number");
+        }
+        else {
+            System.out.println("This is NOT prime number");
+        }
+
+    }
 }
--- src/main/java/java_exercises_1_SDA/Main_1SDA.java
+++ src/main/java/java_exercises_1_SDA/Main_1SDA.java
@@ -2,7 +2,9 @@ package java_exercises_1_SDA;
 
 public class Main_1SDA {
     public static void main(String[] args) {
+        System.out.println("Exercise 1 :");
       Exercise_1.multiplicationTable(5);
-
+        System.out.println("\nExercise 2 :");
+      Exercise_2.checkingPrimeNumber(11);
     }
 }
