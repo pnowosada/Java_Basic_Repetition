0db6177a0f77fdfcd59d38887afa112ee5705a42
Przemek <przem.nowosada@gmail.com>
2020-12-28 21:16:13 +0100

    Exercise1_2SDA

--- src/main/java/java_exersises_2_SDA/Exersise_1.java
+++ src/main/java/java_exersises_2_SDA/Exersise_1.java
@@ -1,7 +1,38 @@
 package java_exersises_2_SDA;
 
+import java.util.Scanner;
+
 public class Exersise_1 {
-    public static void main(String[] args) {
+    public static void weekName(int numberOfWeekDay) {
+
+        switch (numberOfWeekDay){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Write number of day week again : ");
+                break;
+        }
 
     }
-}
+
+    }
+
---
+++ src/main/java/java_exersises_2_SDA/Main_2SDA.java
@@ -0,0 +1,9 @@
+package java_exersises_2_SDA;
+
+public class Main_2SDA {
+    public static void main(String[] args) {
+        System.out.println("Exercise 1 :");
+        Exersise_1.weekName(8);
+
+    }
+}
