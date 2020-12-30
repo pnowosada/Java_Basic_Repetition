61abaa63cb37bf977cbdace534636df84d2a6422
Przemek <przem.nowosada@gmail.com>
2020-12-30 16:50:08 +0100

    Main was added

--- src/main/java/java_exercises_3_SDA/Main_3SDA.java
+++ src/main/java/java_exercises_3_SDA/Main_3SDA.java
@@ -1,2 +1,7 @@
-package java_exercises_3_SDA;public class Main_3SDA {
+package java_exercises_3_SDA;
+
+public class Main_3SDA {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 }
