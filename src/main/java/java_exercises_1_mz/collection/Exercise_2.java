cf4dbc741ba84bc20722845e5ee81245b2d2a5ca
Przemek <przem.nowosada@gmail.com>
2021-01-01 14:54:01 +0100

    Exercise2_1mz

--- src/main/java/java_exercises_1_mz/collection/Exercise_2.java
+++ src/main/java/java_exercises_1_mz/collection/Exercise_2.java
@@ -1,4 +1,35 @@
 package java_exercises_1_mz.collection;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exercise_2 {
+    private List<Integer> exList = new ArrayList<>();
+
+    public List<Integer> addElemList(int... elements) {
+        for (int element : elements) {
+            exList.add(element);
+        }
+        return this.exList;
+    }
+
+    public int listLength() {
+        return exList.size();
+    }
+
+    public Integer getFirst() {
+        return exList.get(0);
+    }
+
+    public Integer getLast() {
+        return exList.get(exList.size() - 1);
+    }
+
+    public void printList() {
+        for (int i = 0; i < exList.size(); i++) {
+            System.out.print(exList.get(i) + " ");
+        }
+        System.out.println();
+    }
+
 }
--- src/main/java/java_exercises_1_mz/collection/Main.java
+++ src/main/java/java_exercises_1_mz/collection/Main.java
@@ -2,7 +2,7 @@ package java_exercises_1_mz.collection;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Exercise 1");
         Exercise_1 myArray=new Exercise_1(25);
         myArray.addToArray();
         myArray.printArray();
@@ -10,5 +10,13 @@ public class Main {
         myArray.printArraySize();
         myArray.multiplication(35);
         myArray.printArray();
+        System.out.println("Exercise 2");
+        Exercise_2 myList=new Exercise_2();
+        myList.addElemList(1,22,53,4);
+        System.out.println(myList.listLength());
+        System.out.println(myList.getFirst());
+        System.out.println(myList.getLast());
+        myList.printList();
+
     }
 }
