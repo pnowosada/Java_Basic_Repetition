4577399f12c0cc5a8cadaa670520eabff2ce6cc0
Przemek <przem.nowosada@gmail.com>
2021-01-02 13:29:32 +0100

    Exercise9-10_1mz

---
+++ src/main/java/java_exercises_1_mz/DateAndTime/Exercise_10.java
@@ -0,0 +1,10 @@
+package java_exercises_1_mz.DateAndTime;
+
+import java.time.LocalDate;
+
+public class Exercise_10 {
+    LocalDate nowDate=LocalDate.now();
+    public void showDate(){
+        System.out.println("Current date: "+nowDate);
+    }
+}
--- src/main/java/java_exercises_1_mz/DateAndTime/Main.java
+++ src/main/java/java_exercises_1_mz/DateAndTime/Main.java
@@ -3,8 +3,10 @@ package java_exercises_1_mz.DateAndTime;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Exercise 9:");
-        Exercise_9 nowTime = new Exercise_9();
-        nowTime.printLocalTime();
-
+        Exercise_9 exercise_9 = new Exercise_9();
+        exercise_9.printLocalTime();
+        System.out.println("Exercise 10:");
+        Exercise_10 exercise_10=new Exercise_10();
+        exercise_10.showDate();
     }
 }
