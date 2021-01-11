b868c0ab3b84d6157dca6b58428f918208cfcbda
Przemek <przem.nowosada@gmail.com>
2021-01-11 23:10:41 +0100

    Business card

---
+++ src/main/java/java_workbook/Exercise_1.java
@@ -0,0 +1,17 @@
+package java_workbook;
+
+public class Exercise_1 {
+    public static void printBusinessCard(){
+        for(int i=0;i<=28;i++){
+            System.out.print("#");
+        }
+        System.out.println("\n#\t\t\t\t\t\t\t#"+
+                           "\n#\t\tJan Kowalski\t\t#"+
+                           "\n#\tul. Grunwaldzka 345/b\t#"+
+                           "\n#\t\t00-950 Warszawa\t\t#"+
+                           "\n#\t\t\t\t\t\t\t#");
+        for(int i=0;i<=28;i++){
+            System.out.print("#");
+        }
+    }
+}
---
+++ src/main/java/java_workbook/Main_Workbook.java
@@ -0,0 +1,7 @@
+package java_workbook;
+
+public class Main_Workbook {
+    public static void main(String[] args) {
+        Exercise_1.printBusinessCard();
+    }
+}
